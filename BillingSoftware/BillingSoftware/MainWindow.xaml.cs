bb441fef25be522c795dd10c50498f07b585cc74
subashselvan <subash.selvan@gmail.com>
2017-09-17 19:33:07 +0530

    Buy item changes

--- BillingSoftware/BillingSoftware/MainWindow.xaml.cs
+++ BillingSoftware/BillingSoftware/MainWindow.xaml.cs
@@ -20,9 +20,48 @@ namespace BillingSoftware
     /// </summary>
     public partial class MainWindow : Window
     {
+       
         public MainWindow()
         {
             InitializeComponent();
+            DataGridTextColumn col1 = new DataGridTextColumn();
+            DataGridTextColumn col2 = new DataGridTextColumn();
+            DataGridTextColumn col3 = new DataGridTextColumn();
+            DataGridTextColumn col4 = new DataGridTextColumn();
+            DataGridTextColumn col5 = new DataGridTextColumn();
+            myDataGrid.Columns.Add(col1);
+            myDataGrid.Columns.Add(col2);
+            myDataGrid.Columns.Add(col3);
+            myDataGrid.Columns.Add(col4);
+            myDataGrid.Columns.Add(col5);
+            col1.Binding = new Binding("No");
+            col2.Binding = new Binding("Item");
+            col3.Binding = new Binding("Weight");
+            col4.Binding = new Binding("Rate_1kg");
+            col5.Binding = new Binding("Amount");
+            col1.Header = "No";
+            col2.Header = "Item";
+            col3.Header = "Weight";
+            col4.Header = "Rate_1kg";
+            col5.Header = "Amount";
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int iValue;
+            int.TryParse(txtBox.Text, out iValue);
+            myDataGrid.Items.Add(new MyData { No = myDataGrid.Items.Count+1, Item = cboBox.Text, Weight = iValue, Rate_1kg = 12, Amount = iValue * 12 });
+        }
+       
     }
+   
+    public class MyData
+    {
+        public int No { get; set; }
+        public string Item { get; set; }
+        public int Weight { get; set; }
+        public double Rate_1kg { get; set; }
+        public double Amount { get; set; }
+    }
+    
 }
